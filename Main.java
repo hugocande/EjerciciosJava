a59cc8ef0582a03a833e2f43148ff841d27d8770
hugocande <84963771+hugocande@users.noreply.github.com>
2021-07-14 13:43:06 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main (String [] args){
-
+        
+        //Ejercicio 1
         Libro libro1 = new Libro("ISBN1","Titulo Libro 1", "Autor 1", 240);
         Libro libro2 = new Libro("ISBN2", "Título Libro 2", "Autor 2", 240);
 
@@ -9,11 +10,9 @@ public class Main {
 
         if (libro1.getNumPaginas()>libro2.getNumPaginas()) {
             System.out.println("El libro 1 tiene más páginas.");
-        }else
-            if (libro1.getNumPaginas()<libro2.getNumPaginas()){
+        } else if (libro1.getNumPaginas()<libro2.getNumPaginas()){
                 System.out.println("El libro 2 tiene más pagínas.");
-            }
-            else System.out.println("Los dos libros tienen las mismas páginas.");
+            } else System.out.println("Los dos libros tienen las mismas páginas.");
         }
     }
 
